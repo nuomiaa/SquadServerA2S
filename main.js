1e9567704490f9dda4f086773662e9455c1eebaa
nuomiaa <nuomiaa@gmail.com>
2023-06-04 23:23:26 +0800

    1

--- main.js
+++ main.js
@@ -6,38 +6,8 @@ let mainInterval = null;
 console.clear();
 console.log('[主线程] 启动');
 
-// (async () => {
-//     console.log('[主线程] 启动');
-//     let ip;
-//     if (arguments.length >= 3) {
-//         ip = arguments[2];
-//         console.log('[主线程] IP：' + ip);
-//     }
 
-//     let servicesList = await require('./util/getServicesList')(ip);
-
-//     for (const services of servicesList) {
-//         console.log(services);
-//         console.log(`[主线程] 创建：${services.nickname}`);
-//         const worker = new Worker('./core/thread.js', {
-//             workerData: services
-//         });
-//         worker.on('error', (error) => {
-//             console.log(`[主线程] 错误：${services.nickname}：`, error);
-//         });
-//         worker.on('exit', (code) => {
-//             worker.removeAllListeners('message');
-//             worker.removeAllListeners('error');
-//             worker.removeAllListeners('exit');
-//             if (code !== 0) console.log(`[主线程] 异常退出：${services.nickname}`);
-//         });
-//         servers.push({
-//             ...services,
-//             worker
-//         });
-//     }
-// })();
-main()
+main();
 async function main() {
     clearInterval(mainInterval);
     const servicesList = await require('./util/getServicesList')();
